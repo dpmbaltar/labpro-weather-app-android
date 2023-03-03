4140f61becba2ff0cf41d9bc9c25b17def57c3ea
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2023-03-03 15:41:13 -0300

    Agregar SwipeRefresh a HourlyWeatherFragment

--- app/src/main/java/com/example/weatherapp/view/HourlyWeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/HourlyWeatherFragment.kt
@@ -8,14 +8,14 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.weatherapp.R
 import com.example.weatherapp.adapter.HourlyWeatherAdapter
 import com.example.weatherapp.databinding.FragmentHourlyWeatherBinding
-import com.example.weatherapp.util.hourMinutes
-import com.example.weatherapp.util.isoDate
-import com.example.weatherapp.util.isoDateTimeSimple
-import com.example.weatherapp.util.weekdayDate
+import com.example.weatherapp.util.*
 import com.example.weatherapp.viewmodel.HourlyWeatherViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import java.util.*
@@ -24,6 +24,8 @@ import java.util.*
 class HourlyWeatherFragment : BottomSheetDialogFragment() {
 
     companion object {
+
+        private val TAG = HourlyWeatherFragment::class.java.simpleName
 
         const val LATITUDE_NAME = "latitude"
         const val LONGITUDE_NAME = "longitude"
@@ -48,6 +50,7 @@ class HourlyWeatherFragment : BottomSheetDialogFragment() {
         }
     }
 
+    private lateinit var refreshView: SwipeRefreshLayout
     private lateinit var viewModel: HourlyWeatherViewModel
     private var _binding: FragmentHourlyWeatherBinding? = null
     private val binding get() = _binding!!
@@ -85,6 +88,8 @@ class HourlyWeatherFragment : BottomSheetDialogFragment() {
         }
 
         arguments?.let { args ->
+            val latitude = args.getDouble(LATITUDE_NAME)
+            val longitude = args.getDouble(LONGITUDE_NAME)
             val date = args.getString(DATE_NAME)
             val sunrise = args.getString(SUNRISE_NAME)
             val sunset = args.getString(SUNSET_NAME)
@@ -99,6 +104,18 @@ class HourlyWeatherFragment : BottomSheetDialogFragment() {
                 binding.sunset.text = sunset
                 Log.d(HourlyWeatherFragment::class.java.simpleName, e.message, e)
             }
+
+            refreshView = binding.swipeRefresh.apply {
+                setOnRefreshListener {
+                    lifecycleScope.launch {
+                        viewModel.loadHourlyWeather(
+                            latitude,
+                            longitude,
+                            Calendar.getInstance().apply { time = date!!.isoDate() }
+                        )
+                    }
+                }
+            }
         }
 
         lifecycleScope.launch {
@@ -107,11 +124,34 @@ class HourlyWeatherFragment : BottomSheetDialogFragment() {
             }
         }
 
+        lifecycleScope.launch { viewModel.error.collect { showError(it) } }
+        lifecycleScope.launch { viewModel.loading.collect { showLoading(it) } }
+
         return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun showLoading(isRefreshing: Boolean = true) {
+        refreshView.isRefreshing = isRefreshing
+    }
+
+    private fun showError(throwable: Throwable?) {
+        Log.d(TAG, throwable?.message, throwable)
+
+        val message = when (throwable) {
+            is ConnectionException -> getString(R.string.connection_exception)
+            is RemoteResponseException -> getString(R.string.remote_response_exception)
+            else -> getString(R.string.unknown_exception)
+        }
+
+        activity?.findViewById<View>(R.id.main_layout)?.let { view ->
+            Snackbar.make(view, message, Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
     }
 }
